fba025deb71091f0d8c7fe55b16fd6137dd9fdef
dypshong <dypshong@gmail.com>
2024-10-04 13:10:12 +0900

    benchmark ckpt

--- test/braket/test_random_circuit.py
+++ test/braket/test_random_circuit.py
@@ -132,7 +132,7 @@ class BraketTest(unittest.TestCase):
     def run_snuqs(self, circ):
         option = {
             'device': 'cuda',
-            'offload': 'cpu',
+            # 'offload': 'cpu',
             # 'path': [ '/dev/nvme0n1', '/dev/nvme1n1', '/dev/nvme2n1', '/dev/nvme3n1', '/dev/nvme4n1', '/dev/nvme5n1', '/dev/nvme6n1', '/dev/nvme7n1', ],
         }
         sim = LocalSimulator(backend="snuqs")
